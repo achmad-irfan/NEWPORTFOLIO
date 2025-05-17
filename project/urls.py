13c525215a08c88ff99529b9d9ddc5abcd155c30
Achmad Irfan <achmad.irfan1@yahoo.com>
2025-05-17 23:52:33 +0700

    hapus slsh di url project

--- project/urls.py
+++ project/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name='app_project'
 urlpatterns = [
     path("", views.indexView.as_view(),name='project'),
-    path("/detail/<slug:slug>", views.detail, name='detail'),
+    path("detail/<slug:slug>", views.detail, name='detail'),
     path('category-<str:category>/',
          views.indexView.as_view(), name='projects_category'),
     path('page-<int:page>/', views.indexView.as_view(), name='project_page'),
