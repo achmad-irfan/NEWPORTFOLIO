d34e87849d352c38fce7f49dacd19569b61d9374
Achmad Irfan <achmad.irfan1@yahoo.com>
2025-05-18 21:43:26 +0700

    setting 4

--- newweb/settings/base.py
+++ newweb/settings/base.py
@@ -115,17 +115,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_DIRS = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')]
+STATIC_URL = "/static/"
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-print(f"BASE_DIR = {MEDIA_DIRS}")
-print(f"SECRET_KEY = {SECRET_KEY}")
