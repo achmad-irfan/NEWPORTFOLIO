d5e87f206ede084022c319546fe729c7048696f8
Achmad Irfan <achmad.irfan1@yahoo.com>
2025-05-18 20:45:33 +0700

    wsgi

--- newweb/wsgi.py
+++ newweb/wsgi.py
@@ -9,6 +9,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
+from dotenv import load_dotenv
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newweb.settings")
