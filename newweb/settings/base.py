17977c699cb610ad0172755f7b3914d1a8d67026
Achmad Irfan <achmad.irfan1@yahoo.com>
2025-05-18 21:15:41 +0700

    setting

--- newweb/settings/base.py
+++ newweb/settings/base.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_DIRS = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
