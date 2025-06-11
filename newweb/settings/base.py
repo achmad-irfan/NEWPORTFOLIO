58b8a47d0612b45541a51ccef156c55c418b6093
Achmad Irfan <achmad.irfan1@yahoo.com>
2025-06-11 10:53:58 +0700

    ubah static url

--- newweb/settings/base.py
+++ newweb/settings/base.py
@@ -118,7 +118,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_DIRS = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = "/static/"
+STATIC_URL = "/portfolio_static/"
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')]
 
