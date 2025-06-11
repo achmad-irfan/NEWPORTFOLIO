3daccdc26084c36964ac4fec644255144a128c68
Achmad Irfan <achmad.irfan1@yahoo.com>
2025-06-11 13:33:41 +0700

    ubah static static dir

--- newweb/settings/base.py
+++ newweb/settings/base.py
@@ -119,8 +119,8 @@ MEDIA_URL = '/portfolio_media/'
 MEDIA_DIRS = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = "/portfolio_static/"
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
