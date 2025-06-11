afcd1049154edbeef0f9e6ed2d456d8b148f3b19
Achmad Irfan <achmad.irfan1@yahoo.com>
2025-06-11 12:11:20 +0700

    ubah static root folder 3

--- newweb/settings/base.py
+++ newweb/settings/base.py
@@ -115,10 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/portfolio_media/'
 MEDIA_DIRS = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = "/static/"
+STATIC_URL = "/portfolio_static/"
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')]
 
